43819d227d89406ba386345cb22f66631075c5b5
Nazlı <nazliacnmn@hotmail.com>
2026-07-23 19:42:25 +0300

    Add PostFileAsync method to IApiService

--- EventTicket.UI/Services/IApiService.cs
+++ EventTicket.UI/Services/IApiService.cs
@@ -1,9 +1,12 @@
-﻿namespace EventTicket.UI.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace EventTicket.UI.Services;
 
 public interface IApiService
 {
     Task<T?> GetAsync<T>(string endpoint);
     Task<T?> PostAsync<T>(string endpoint, object data);
+    Task<T?> PostFileAsync<T>(string endpoint, IFormFile file);
     Task<T?> PatchAsync<T>(string endpoint);
     Task PutAsync(string endpoint, object data);
     Task<T?> PutAsync<T>(string endpoint, object data);
